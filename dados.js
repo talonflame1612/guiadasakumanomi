53030db35ea22f5ea3833976398f8b38a10b931c
talonflame1612 <luizh.evangelista2508@gmail.com>
2024-09-07 19:52:15 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,53 @@
+let dados = [
+    {
+        titulo: "Hana hana nomi",
+        descricao: "A Hana Hana no Mi é uma Akuma no Mi do tipo Paramecia que confere ao seu usuário a habilidade de brotar partes de seu corpo em qualquer superfície sólida. Essa fruta permite uma gama incrível de possibilidades, desde criar membros extras para atacar até manipular o ambiente à sua volta, tornando-a uma arma formidável nas mãos de um combatente habilidoso.  \n Usuário atual: Nico Robin \n Tipo: Paramecia ",
+        tags:"flor nico robin",
+        link:"https://onepiece.fandom.com/pt/wiki/Hana_Hana_no_Mi"
+    },
+    {
+        titulo: "Gomu Gomu no Mi",
+        descricao: "A Gomu Gomu no Mi é uma Akuma no Mi do tipo Paramecia que confere ao usuário a habilidade de esticar e deformar seu corpo como se fosse borracha. Essa fruta permite uma grande variedade de ataques e movimentos ágeis. \nUsuário atual: Monkey D. Luffy\nTipo: Paramecia.",
+        tags:"luffy borracha hito hito nika",
+        link:"https://onepiece.fandom.com/pt/wiki/Gomu_Gomu_no_Mi"
+    },
+    {
+        titulo: "Mera Mera no Mi",
+        descricao: "A Mera Mera no Mi é uma Akuma no Mi do tipo Logia que concede ao usuário o poder de criar e controlar o fogo. Essa fruta permite gerar chamas de diferentes temperaturas e formas, além de conferir resistência ao calor. \n Usuário atual: Sabo \n Tipo: Logia",
+        tags:"ace fogo buraco ",
+        link:"https://onepiece.fandom.com/pt/wiki/Mera_Mera_no_Mi"
+    },
+
+
+    {
+        titulo: "Ope Ope no Mi",
+        descricao: "A Ope Ope no Mi é uma Akuma no Mi do tipo Paramecia que concede ao usuário o poder de realizar cirurgias em qualquer coisa, incluindo seres vivos. Essa fruta permite trocar corpos, criar barreiras e até mesmo realizar a cirurgia da imortalidade.\nUsuário atual: Trafalgar Law\nTipo: Paramecia",
+        tags:"law operaçao coraçao imortalidade",
+        link:"https://onepiece.fandom.com/pt/wiki/Ope_Ope_no_Mi?so=search"
+    },
+    {
+        titulo: "Yami Yami no Mi",
+        descricao: "A Yami Yami no Mi é uma Akuma no Mi do tipo Logia que concede ao usuário o poder de manipular as trevas. Essa fruta permite absorver qualquer ataque, criar portais dimensionais e anular os poderes de outras Akuma no Mi.\nUsuário atual: Marshall D. Teach (Barbanegra)\nTipo: Logia",
+        tags:"barba negra escuridao ",
+        link:"https://onepiece.fandom.com/pt/wiki/Yami_Yami_no_Mi?so=search"
+    },
+    {
+        titulo: "Mochi Mochi no Mi",
+        descricao: "A Mochi Mochi no Mi é uma Akuma no Mi do tipo Paramecia que confere ao usuário a habilidade de transformar seu corpo e tudo que tocar em mochi. Essa fruta permite uma grande variedade de ataques pegajosos e esticados, além de conferir uma incrível elasticidade ao usuário.\nUsuário atual: Charlotte Katakuri\nTipo: Paramecia",
+        tags:"big mon katakuri charlote ",
+        link:"https://onepiece.fandom.com/pt/wiki/Mochi_Mochi_no_Mi?so=search"
+    },
+    {
+        titulo: "Pika Pika no Mi",
+        descricao: "A Pika Pika no Mi é uma Akuma no Mi do tipo Logia que concede ao usuário o poder de manipular a luz. Essa fruta permite criar construções de luz, viajar à velocidade da luz e até mesmo se tornar intangível.\nUsuário atual: Kizaru\nTipo: Logia",
+        tags:"pika luz velocidade  kizaru almirante",
+        link:"https://onepiece.fandom.com/pt/wiki/Pika_Pika_no_Mi?so=search"
+    },
+    {
+        titulo: "Yuki Yuki no Mi",
+        descricao: "A Yuki Yuki no Mi é uma Akuma no Mi do tipo Logia que concede ao usuário o poder de criar e controlar a neve. Essa fruta permite criar nevascas, congelar objetos e até mesmo voar sobre nuvens de neve.\nUsuário atual: Monet\nTipo: Logia",
+        tags:"neve monet",
+        link:"https://onepiece.fandom.com/pt/wiki/Yuki_Yuki_no_Mi?so=search"
+    }
+
+];
